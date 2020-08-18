749707cd2f6897edf9df51cf3abf06fe732e9c9f
Will Fiore <w.p.fiore@gmail.com>
2020-08-18 21:05:58 +0100

    Implement command line parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cstdint>
 #include <deque>
 #include <iomanip>
@@ -131,6 +132,7 @@ struct CommandLineOption {
         HELP,
         ROOT_NAMESPACE,
         PRINT_OUTPUT_FILES,
+        MAX
     } id;
 
     std::string long_name;
@@ -144,13 +146,14 @@ struct CommandLineOption {
     } type;
 };
 
+// Must be in same order as Id enum
 CommandLineOption command_line_options[] = {
     CommandLineOption {
         .id = CommandLineOption::Id::HELP,
         .long_name = "help",
         .short_name = "h",
         .description = "print this summary",
-        .default = "",
+        .default = "0",
         .type = CommandLineOption::Type::BOOLEAN,
     },
     CommandLineOption {
@@ -166,7 +169,7 @@ CommandLineOption command_line_options[] = {
         .long_name = "print-output-files",
         .short_name = "p",
         .description = "print absolute paths of output source files\ne.g. to feed into build systems",
-        .default = "",
+        .default = "0",
         .type = CommandLineOption::Type::BOOLEAN,
     },
 };
@@ -222,7 +225,7 @@ Options:
             }
         }
 
-        if (!flag.default.empty()) {
+        if (!flag.default.empty() && flag.type != CommandLineOption::Type::BOOLEAN) {
             ss << " [default: \"" << flag.default << "\"]";
         }
 
@@ -241,7 +244,12 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-    std::unordered_map<CommandLineOption::Id, std::string> args;
+    std::array<std::string, (size_t)CommandLineOption::Id::MAX> args;
+
+    // Populate default args
+    for (size_t i = 0; i < (size_t)CommandLineOption::Id::MAX; ++i) {
+        args[i] = command_line_options[i].default;
+    }
 
     bool print_help = false;
     std::string unknown_arg;
@@ -270,7 +278,7 @@ int main(int argc, const char* argv[]) {
         }
 
         if (command_line_option->type == CommandLineOption::Type::BOOLEAN) {
-            args[command_line_option->id] = "";
+            args[static_cast<size_t>(command_line_option->id)] = "1";
         }
         else if (command_line_option->type == CommandLineOption::Type::STRING) {
             // Read ahead
@@ -281,12 +289,8 @@ int main(int argc, const char* argv[]) {
             }
 
             std::string val = argv[i];
-            args[command_line_option->id] = val;
+            args[(size_t)command_line_option->id] = val;
         }
-    }
-
-    for (const auto& a : args) {
-        printf("Arg %d: %s\n", static_cast<int>(a.first), a.second.c_str());
     }
 
     if (print_help) {
@@ -299,15 +303,16 @@ int main(int argc, const char* argv[]) {
         return 1;
     }
 
-    std::string root_input_path  = NormalizeDirectoryString(argv[1]);
+    DWORD cwd_length = ::GetCurrentDirectory(0, NULL);
+    std::string cwd(cwd_length, '\0');
+    ::GetCurrentDirectory(cwd_length, cwd.data());
+    cwd.back() = '\\'; // replace null terminator with backslash
 
-    std::string root_output_path = NormalizeDirectoryString(argv[2]);
+    std::string root_input_path  = NormalizeDirectoryString(argv[argc - 2]);
+    std::string root_output_path = NormalizeDirectoryString(argv[argc - 1]);
 
     std::vector<std::string> root_input_directories = SplitString(root_input_path, "\\");
     std::vector<std::string> root_output_directories = SplitString(root_output_path, "\\");
-
-    printf("Input path: %s\n", root_input_path.c_str());
-    printf("Output path: %s\n", root_output_path.c_str());
 
     std::vector<std::string> open_directory_list{ root_input_path };
 
@@ -321,8 +326,9 @@ int main(int argc, const char* argv[]) {
 #include <array>
 #include <cstdint>
 
-namespace Bin {
-)";
+namespace )";
+
+    ss_header_file << args[(size_t)CommandLineOption::Id::ROOT_NAMESPACE] << " {\n\n";
 
     while (!open_directory_list.empty()) {
         std::string dir = NormalizeDirectoryString(open_directory_list.back());
@@ -376,8 +382,9 @@ namespace Bin {
 #include <array>
 #include <cstdint>
 
-namespace Bin {
-)";
+namespace )";
+
+                ss_cpp_file << args[(size_t)CommandLineOption::Id::ROOT_NAMESPACE] << " {\n";
 
                 for (const auto& n : namespaces) {
                     ss_cpp_file << "namespace " << n << " {\n";
@@ -435,7 +442,12 @@ namespace Bin {
                     ::CreateDirectory(current_directory_path.c_str(), NULL);
                 }
 
+                std::string path = current_directory_path + find_data.cFileName + ".cpp";
                 ::WriteFile(current_directory_path + find_data.cFileName + ".cpp", output_data);
+
+                if (args[(size_t)CommandLineOption::Id::PRINT_OUTPUT_FILES] == "1") {
+                    printf("%s%s\n", cwd.c_str(), path.c_str());
+                }
 
                 // Populate namespaces for header
                 for (size_t i = 0; i < header_namespaces.size(); ++i) {
